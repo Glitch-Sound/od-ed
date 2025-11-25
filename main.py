b7bda6f42360be4b07c82f185693dc08f9e09cd5
Glitch-Sound <uid.glitch@gmail.com>
2025-11-26 08:19:03 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from efficientdet import ob_efficientdet_test, ob_efficientdet_train
+
+
+def main():
+    ob_efficientdet_train()
+    ob_efficientdet_test()
+
+
+if __name__ == "__main__":
+    main()
